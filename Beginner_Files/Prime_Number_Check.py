f1bd3b6f575be53c0a75e32dd151a8f931982ba2
sakhan <saquib.khan@here.com>
2023-01-21 20:00:37 +0530

    Updated logic of 'Prime_Number_Check.py'

--- Beginner_Files/Prime_Number_Check.py
+++ Beginner_Files/Prime_Number_Check.py
@@ -1,11 +1,10 @@
 #Write your code below this line 👇
 def prime_checker(number):
     is_prime = True
-    for i in range(int((number/2)+1)):
-        for j in range(int((number/2)+1)):
-            if i*j == number:
-                is_prime = False
-                break
+    for i in range(2, number):
+        if number%i == 0:
+            is_prime = False
+
     if is_prime:
         print("It's a prime number.")
     else:
